3978f4e0057c7e301133611de128d840f64b2129
guooo <guooo@guooo-PC>
2016-12-02 13:28:37 +0800

    add new moudle

---
+++ serviceLayer/src/main/java/com/guooo/boot/I/acc/HelloDubboService.java
@@ -0,0 +1,14 @@
+package com.guooo.boot.I.acc;
+
+/**
+ * @author guooo
+ *
+ */
+public interface HelloDubboService {
+
+	/**
+	 * @param words
+	 * @return
+	 */
+	String sayHello(String words);
+}
---
+++ serviceLayer/src/main/java/com/guooo/boot/I/order/OrderService.java
@@ -0,0 +1,14 @@
+package com.guooo.boot.I.order;
+
+/**
+ * @author guooo
+ *
+ */
+public interface OrderService {
+
+	/**
+	 * @param serialNo
+	 * @return
+	 */
+	String queryOrder(String serialNo);
+}
