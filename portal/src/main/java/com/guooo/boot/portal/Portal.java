df1af84d3c3006fdae7ef78fe153afd65f2fa4d9
guooo <guooo@guooo-PC>
2016-12-12 11:58:29 +0800

    代码变更

--- portal/src/main/java/com/guooo/boot/portal/Portal.java
+++ portal/src/main/java/com/guooo/boot/portal/Portal.java
@@ -1,11 +1,11 @@
 package com.guooo.boot.portal;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import java.io.IOException;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.guooo.boot.I.acc.HelloDubboService;
 
 /**
  * Entrypoint
@@ -13,13 +13,17 @@ import org.springframework.context.annotation.ImportResource;
  *
  */
 @SpringBootApplication
-@ComponentScan("com.guooo.boot.portal.web.*,com.guooo.boot.I.*")
-@ImportResource({"classpath:dubbo-portal-consumer.xml"})
-@EnableAutoConfiguration
-@Configuration
 public class Portal {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Portal.class, args);
-	}
+	public static void main(String[] args){
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"classpath:dubbo-portal-consumer.xml"});
+        HelloDubboService helloService =  (HelloDubboService) ctx.getBean("helloDubboService");
+        System.out.println(helloService.sayHello("lxy"));
+        try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 }
